9bdc80377664ad2f078b59cefcba2d7dadfd1ea4
humahmed1 <73528337+humahmed1@users.noreply.github.com>
2021-01-05 15:40:21 +0000

    Use Maven Surefire 2.19.1

--- target/classes/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS subscription;
-
-CREATE TABLE subscription (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    price FLOAT NOT NULL,
-    date INT NOT NULL,
-    account VARCHAR(255)
-);
--- target/test-classes/com/example/project/karateTests/KarateTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.karateTests;
-
-import com.intuit.karate.junit4.Karate;
-import org.junit.runner.RunWith;
-
-@RunWith(Karate.class)
-class KarateTest {
-
-}
